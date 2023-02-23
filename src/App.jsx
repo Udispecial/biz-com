c7c8d5697a5839ecfe7036e5dd8e26e0fe953e22
Uduak Edet <edet.uduak@techvibesltd.com>
2023-02-23 10:59:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./style";
+import {
+  Billing,
+  Bussiness,
+  Button,
+  CardDeal,
+  CTA,
+  ClientSection,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  NavBar,
+  Stats,
+  Testimonials,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart} `}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Bussiness />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <ClientSection />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Bussiness from "./Bussiness";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import ClientSection from "./ClientSection";
+import CTA from "./CTA";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Billing,
+  Bussiness,
+  Button,
+  CardDeal,
+  CTA,
+  ClientSection,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  NavBar,
+  Stats,
+  Testimonials,
+};
